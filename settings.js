c16c25f93e8369a5af8ad3211013c5b524432893
fahrilar <96267756+FAHMEN@users.noreply.github.com>
2024-12-17 11:27:52 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,18 +1,14 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
-/*
-	* Create By Naze
-	* Follow https://github.com/nazedev
-	* Whatsapp : https://whatsapp.com/channel/0029VaWOkNm7DAWtkvkJBK43
-*/
-
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
-global.owner = ['6282113821188','0']
-global.packname = 'Bot WhatsApp'
-global.author = 'Nazedev'
-global.botname = 'Naze Bot'
+global.owner = ['6285189297059']
+global.packname = 'Fazbot'
+global.fazbot = 'WhatsApp-bot'
+global.author = 'Fahmen\n\nBot:79269175361'
+global.fazz = 'Fazbot'
+global.botname = 'Fazbot'
 global.listprefix = ['+','!']
 global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
 global.tempatDB = 'database.json'
@@ -20,22 +16,22 @@ global.pairing_code = true
 
 global.fake = {
 	anonim: 'https://telegra.ph/file/95670d63378f7f4210f03.png',
-	thumbnailUrl: 'https://telegra.ph/file/fe4843a1261fc414542c4.jpg',
-	thumbnail: fs.readFileSync('./src/media/naze.png'),
+	thumbnailUrl: 'https://drive.usercontent.google.com/download?id=1-3LW80t18wDldXom1Mu1Ta78wD0T2m7u&authuser=0',
+	thumbnail: fs.readFileSync('./src/media/faz.png'),
 	docs: fs.readFileSync('./src/media/fake.pdf'),
 	listfakedocs: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/vnd.openxmlformats-officedocument.presentationml.presentation','application/vnd.openxmlformats-officedocument.wordprocessingml.document','application/pdf'],
 }
 
 global.my = {
-	yt: 'https://youtube.com/c/Nazedev',
-	gh: 'https://github.com/nazedev',
-	gc: 'https://chat.whatsapp.com/Hx9vcBVhbc04KLVGPFtH2R',
-	ch: '120363250409960161@newsletter',
+	yt: 'https://donatesuport.rf.gd/',
+	gh: 'https://donatesuport.rf.gd/',
+	gc: 'https://whatsapp.com/channel/0029Vaz7yJFHVvTYroi7o227',
+	ch: '0029Vaz7yJFHVvTYroi7o227@newsletter',
 }
 
 global.limit = {
-	free: 100,
-	premium: 999,
+	free: 80,
+	premium: 1000,
 	vip: 'VIP'
 }
 
@@ -51,9 +47,9 @@ global.mess = {
 	admin: 'Fitur Khusus Admin!',
 	botAdmin: 'Bot Bukan Admin!',
 	group: 'Gunakan Di Group!',
-	private: 'Gunakan Di Privat Chat!',
+	private: 'Gunakan Di Chat Pribadi!',
 	prem: 'Khusus User Premium!',
-	wait: 'Loading...',
+	wait: 'Loading... . .',
 	error: 'Error!',
 	done: 'Done'
 }
